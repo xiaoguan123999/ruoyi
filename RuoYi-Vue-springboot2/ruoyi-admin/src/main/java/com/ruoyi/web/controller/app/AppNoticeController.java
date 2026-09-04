940735feef8070802575f60e732174d4fa329463
小关 <xiaoguan3937@gmail.com>
2026-09-04 16:34:40 +0800

    feat:app公告查询html

--- RuoYi-Vue-springboot2/ruoyi-admin/src/main/java/com/ruoyi/web/controller/app/AppNoticeController.java
+++ RuoYi-Vue-springboot2/ruoyi-admin/src/main/java/com/ruoyi/web/controller/app/AppNoticeController.java
@@ -8,13 +8,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.util.HtmlUtils;
 import com.ruoyi.biz.api.AppNoticeDetailResult;
 import com.ruoyi.biz.api.AppNoticeItem;
 import com.ruoyi.biz.api.AppNoticeListResult;
 import com.ruoyi.common.annotation.Anonymous;
 import com.ruoyi.common.core.controller.BaseController;
-import com.ruoyi.common.utils.StringUtils;
 import com.ruoyi.system.domain.SysNotice;
 import com.ruoyi.system.service.ISysNoticeService;
 import io.swagger.annotations.Api;
@@ -35,7 +33,7 @@ public class AppNoticeController extends BaseController
     private ISysNoticeService noticeService;
 
     @Anonymous
-    @ApiOperation(value = "公告列表", notes = "不传 noticeType 返回通知+公告。传 1 仅通知，传 2 仅公告。每条含 noticeContent 纯文本。")
+    @ApiOperation(value = "公告列表", notes = "不传 noticeType 返回通知+公告。传 1 仅通知，传 2 仅公告。noticeContent 为后台富文本 HTML。")
     @GetMapping
     public AppNoticeListResult list(
             @ApiParam("1通知 2公告，不传则两种都返回") @RequestParam(value = "noticeType", required = false) String noticeType)
@@ -62,7 +60,7 @@ public class AppNoticeController extends BaseController
     }
 
     @Anonymous
-    @ApiOperation(value = "公告详情", notes = "data.noticeContent 为纯文本。")
+    @ApiOperation(value = "公告详情", notes = "data.noticeContent 为后台富文本 HTML。")
     @GetMapping("/{noticeId}")
     public AppNoticeDetailResult detail(@PathVariable Long noticeId)
     {
@@ -84,7 +82,7 @@ public class AppNoticeController extends BaseController
         item.setCreateTime(notice.getCreateTime());
         if (withContent)
         {
-            item.setNoticeContent(toPlainText(notice.getNoticeContent()));
+            item.setNoticeContent(notice.getNoticeContent());
         }
         return item;
     }
@@ -92,21 +90,5 @@ public class AppNoticeController extends BaseController
     private boolean isAppNoticeType(String noticeType)
     {
         return TYPE_NOTICE.equals(noticeType) || TYPE_ANNOUNCEMENT.equals(noticeType);
-    }
-
-    private String toPlainText(String html)
-    {
-        if (StringUtils.isEmpty(html))
-        {
-            return "";
-        }
-        String text = html.replaceAll("(?i)<script[\\s\\S]*?</script>", "")
-                .replaceAll("(?i)<style[\\s\\S]*?</style>", "")
-                .replaceAll("(?i)<br\\s*/?>", "\n")
-                .replaceAll("(?i)</p>", "\n")
-                .replaceAll("(?i)</div>", "\n")
-                .replaceAll("(?i)<[^>]+>", "");
-        text = HtmlUtils.htmlUnescape(text).replace('\u00A0', ' ');
-        return text.replaceAll("[ \\t]+", " ").replaceAll("\\n{3,}", "\n\n").trim();
     }
 }
